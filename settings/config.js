c29a98f707c9fae6f5aac9754dfa99cb067bc773
iyedzitouni-prog <iyed.zitouni@icmarconisgv.edu.it>
2025-12-14 04:48:23 +0100

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   token: "YOUR_BOT_TOKEN_HERE",
-  clientId: "",
-  guildId: "",
+  clientId: "1449546254206898398",
+  guildId: "833089968586620978",
 
-  ticketCategory: "",
-  supportRole: "",
-  logsChannel: ""
+  ticketCategory: "1449209437377003600",
+  supportRole: "1449593881585913998",
+  logsChannel: "1449593772597055720"
 };
